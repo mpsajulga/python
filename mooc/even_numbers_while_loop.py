bcd296284b9511bb90accc34d14616b9d275170f
Mychal Sajulga <mychalphillipsajulga@gmail.com>
2025-05-07 13:29:05 +0800

    answered two exercises.

---
+++ mooc/even_numbers_while_loop.py
@@ -0,0 +1,5 @@
+even_number = 2
+
+while even_number < 32:
+    print(even_number)
+    even_number += 2
---
+++ mooc/fix_the_code_countdown.py
@@ -0,0 +1,8 @@
+print("Are you ready?")
+numbers = int(input("Please type in a number: "))
+
+while numbers > 0:
+    print(numbers)
+    numbers -= 1
+    
+print("Now!")
