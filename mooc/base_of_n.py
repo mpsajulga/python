f73cc88c402d533353beafdbeda8530704606055
Mychal Sajulga <mychalphillipsajulga@gmail.com>
2025-05-07 14:01:04 +0800

    answered ex. 'base of n'

---
+++ mooc/base_of_n.py
@@ -0,0 +1,8 @@
+upper_limit = int(input("Upper limit: "))
+base_number = int(input("Base: "))
+#upper_limit = 20
+initial_number = 1
+
+while initial_number <= upper_limit:
+    print(initial_number)
+    initial_number *= base_number
